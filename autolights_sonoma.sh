3b47a82a24845d5da3ced0a2382cd77cba1990f3
Todd Nine <tnine@twilio.com>
2024-05-07 15:51:13 -0600

    Pushed fix for local light

--- autolights_sonoma.sh
+++ autolights_sonoma.sh
@@ -3,16 +3,17 @@
 # Begin looking at the system log via the steam sub-command. Using a --predicate and filtering by the correct and pull out the camera event 
 
 echo "Monitoring camera stream."
-
-log stream --predicate 'composedMessage contains "AVFCapture"' | while read line; do
+# ANE0: ANE_UserClientOpen_gated -- on command
+#  ANE0 H11ANEIn::ANE_UserClientClosed_gated -- off command
+log stream --predicate 'composedMessage contains "ANE_UserClient"' | while read line; do
   
   # The camera start event has been caught and is set to 'On', turn the light on
-  if echo "$line" | grep -qE "AVCaptureSession_Tundra startRunning"; then
+  if echo "$line" | grep -qE "ANE_UserClientOpen_gated"; then
 	
 	echo "Camera has been activated, turn on the light."
 		
 	# Light 1 - change IP to your first lights IP
-	curl --location --request PUT 'http://192.168.50.192:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":40,"temperature":162,"on":1}],"numberOfLights":1}'
+	curl --location --request PUT 'http://10.10.0.72:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":35,"temperature":200,"on":1}],"numberOfLights":1}'
 
 	#Light 2 (remove if you only have 1 light), change IP to your second light
 	#curl --location --request PUT 'http://192.168.86.38:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":40,"temperature":162,"on":1}],"numberOfLights":1}'
@@ -20,11 +21,11 @@ log stream --predicate 'composedMessage contains "AVFCapture"' | while read line
   fi
 
   # If we catch a camera stop event, turn the light off.
-  if echo "$line" | grep -qE "AVCaptureSession_Tundra stopRunning"; then
+  if echo "$line" | grep -qE "ANE_UserClientClosed_gated"; then
 	echo "Camera shut down, turn off the light."
 	
 	#Light 1 - set to off
-	curl --location --request PUT 'http://192.168.50.192:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":40,"temperature":162,"on":0}],"numberOfLights":2}'
+	curl --location --request PUT 'http://10.10.0.72:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":35,"temperature":200,"on":0}],"numberOfLights":2}'
 
 	#Light 2 - set to off
 	#curl --location --request PUT 'http://192.168.86.38:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":40,"temperature":162,"on":0}],"numberOfLights":2}'
