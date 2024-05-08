fcf9befa934bda675e7c8bd9506f129ff25eef5e
Todd Nine <tnine@twilio.com>
2024-05-08 13:14:38 -0600

    Fixes events to use ANE_Power and zoom only

--- autolights_sonoma.sh
+++ autolights_sonoma.sh
@@ -3,14 +3,18 @@
 # Begin looking at the system log via the steam sub-command. Using a --predicate and filtering by the correct and pull out the camera event 
 
 echo "Monitoring camera stream."
-# ANE0: ANE_UserClientOpen_gated -- on command
-#  ANE0 H11ANEIn::ANE_UserClientClosed_gated -- off command
-log stream --predicate 'composedMessage contains "ANE_UserClient"' | while read line; do
+# ANE0: ANE_UserClientOpen_gated
+#  ANE0 H11ANEIn::ANE_UserClientClosed_gated
+
+#ANE0: ANE_PowerOn_gated:
+#ANE0: ANE_PowerOff_gated:
+log stream --predicate 'process == "kernel" && composedMessage contains "ANE_Power"' | while read line; do
   
   # The camera start event has been caught and is set to 'On', turn the light on
-  if echo "$line" | grep -qE "ANE_UserClientOpen_gated"; then
+  if echo "$line" | grep -qE "ANE0: ANE_PowerOn_gated: clientname:zoom.us isPowered: 1"; then
 	
 	echo "Camera has been activated, turn on the light."
+	echo "$line"
 		
 	# Light 1 - change IP to your first lights IP
 	curl --location --request PUT 'http://10.10.0.72:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":35,"temperature":200,"on":1}],"numberOfLights":1}'
@@ -21,8 +25,9 @@ log stream --predicate 'composedMessage contains "ANE_UserClient"' | while read 
   fi
 
   # If we catch a camera stop event, turn the light off.
-  if echo "$line" | grep -qE "ANE_UserClientClosed_gated"; then
+  if echo "$line" | grep -qE "ANE0: ANE_PowerOff_gated: clientname:zoom.us isPowered: 1"; then
 	echo "Camera shut down, turn off the light."
+	echo "$line"
 	
 	#Light 1 - set to off
 	curl --location --request PUT 'http://10.10.0.72:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":35,"temperature":200,"on":0}],"numberOfLights":2}'
